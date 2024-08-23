6b73b8fc3945de7ebd06508ca99b1b750695475b
Ata Parlar <ataparlar@leodrive.ai>
2024-08-23 20:34:22 +0300

    start to image_projection
    Signed-off-by: Ata Parlar <ataparlar@leodrive.ai>

--- include/loam_feature_localization/image_projection.hpp
+++ include/loam_feature_localization/image_projection.hpp
@@ -65,13 +65,71 @@ public:
   using SharedPtr = std::shared_ptr<ImageProjection>;
   using ConstSharedPtr = const std::shared_ptr<ImageProjection>;
 
-  explicit ImageProjection();
+  explicit ImageProjection(int N_SCAN, int Horizon_SCAN);
+
 private:
-  std::mutex imuLock;
-  std::mutex odoLock;
-  
+  std::mutex imu_lock_;
+  std::mutex odom_lock_;
+
+  Utils::SharedPtr utils_;
+
+  int n_scan_;
+  int horizon_scan_;
+
+  std::deque<sensor_msgs::msg::Imu> imu_queue_;
+  std::deque<nav_msgs::msg::Odometry> odom_queue_;
+  std::deque<pcl::PointCloud<PointType>> cloud_queue_;
+
+  pcl::PointCloud<PointType> current_cloud_msg_;
+
+  double * imu_time_ = new double[queueLength];
+  double * imu_rot_x_ = new double[queueLength];
+  double * imu_rot_y_ = new double[queueLength];
+  double * imu_rot_z_ = new double[queueLength];
+
+  int imu_pointer_cur_;
+  bool first_point_flag_;
+  Eigen::Affine3f trans_start_inverse_;
+
+  pcl::PointCloud<PointXYZIRT>::Ptr laser_cloud_in_;
+  //  pcl::PointCloud<OusterPointXYZIRT>::Ptr tmpOusterCloudIn;
+  pcl::PointCloud<PointType>::Ptr full_cloud_;
+  pcl::PointCloud<PointType>::Ptr extracted_cloud_;
+
+  int ring_flag_ = 0;
+  int deskew_flag_;
+  cv::Mat range_mat_;
+
+  bool odom_deskew_flag_;
+  float odom_incre_x_;
+  float odom_incre_y_;
+  float odom_incre_z_;
+
+  Utils::CloudInfo cloud_info_;
+  double time_scan_cur_;
+  double time_scan_end_;
+  std_msgs::msg::Header cloud_header_;
+
+  std::vector<int> column_ind_count_vec_;
+
+  void allocate_memory();
+  void reset_parameters();
+  void imu_handler(const sensor_msgs::msg::Imu::SharedPtr imuMsg);
+  void odometry_handler(const nav_msgs::msg::Odometry::SharedPtr odometryMsg);
+  void cloud_handler(const sensor_msgs::msg::PointCloud2::SharedPtr laserCloudMsg);
+  bool cache_point_cloud(const sensor_msgs::msg::PointCloud2::SharedPtr & laserCloudMsg);
+  bool deskew_info();
+  void imu_deskew_info();
+  void odom_deskew_info();
+  void find_rotation(double pointTime, float * rotXCur, float * rotYCur, float * rotZCur);
+  void find_position(double relTime, float * posXCur, float * posYCur, float * posZCur);
+  PointType deskew_point(PointType * point, double relTime);
+  void project_point_cloud();
+  void cloud_extraction();
+  void publish_clouds();
+
 };
-}
+} // namespace loam_feature_localization
 
 
 #endif  // LOAM_FEATURE_LOCALIZATION__IMAGE_PROJECTION_HPP_
--- src/image_projection.cpp
+++ src/image_projection.cpp
@@ -16,8 +16,185 @@
 
 namespace loam_feature_localization
 {
-ImageProjection::ImageProjection()
-{
 
+ImageProjection::ImageProjection(int N_SCAN, int Horizon_SCAN)
+{
+    n_scan_ = N_SCAN;
+    horizon_scan_ = Horizon_SCAN;
+
+    utils_ = std::make_shared<Utils>();
+
+    allocate_memory();
+    reset_parameters();
+
+    pcl::console::setVerbosityLevel(pcl::console::L_ERROR);
 }
+
+void ImageProjection::allocate_memory()
+{
+  laser_cloud_in_.reset(new pcl::PointCloud<PointXYZIRT>());
+  //    tmpOusterCloudIn.reset(new pcl::PointCloud<OusterPointXYZIRT>());
+  full_cloud_.reset(new pcl::PointCloud<PointType>());
+  extracted_cloud_.reset(new pcl::PointCloud<PointType>());
+
+  full_cloud_->points.resize(n_scan_ * horizon_scan_);
+
+  cloud_info_.start_ring_index.assign(n_scan_, 0);
+  cloud_info_.end_ring_index.assign(n_scan_, 0);
+
+  cloud_info_.point_col_index.assign(n_scan_ * horizon_scan_, 0);
+  cloud_info_.point_range.assign(n_scan_ * horizon_scan_, 0);
+
+  reset_parameters();
+}
+
+void ImageProjection::reset_parameters()
+{
+  laser_cloud_in_->clear();
+  extracted_cloud_->clear();
+  // reset range matrix for range image projection
+  range_mat_ = cv::Mat(n_scan_, horizon_scan_, CV_32F, cv::Scalar::all(FLT_MAX));
+
+  imu_pointer_cur_ = 0;
+  first_point_flag_ = true;
+  odom_deskew_flag_ = false;
+
+  for (int i = 0; i < queueLength; ++i) {
+    imu_time_[i] = 0;
+    imu_rot_x_[i] = 0;
+    imu_rot_y_[i] = 0;
+    imu_rot_z_[i] = 0;
+  }
+}
+
+void ImageProjection::imu_handler(const sensor_msgs::msg::Imu::SharedPtr imuMsg)
+{
+  sensor_msgs::msg::Imu this_imu_ = utils_->imu_converter(*imuMsg);
+
+  std::lock_guard<std::mutex> lock1(imu_lock_);
+  imu_queue_.push_back(this_imu_);
+}
+
+void ImageProjection::odometry_handler(const nav_msgs::msg::Odometry::SharedPtr odometryMsg)
+{
+  std::lock_guard<std::mutex> lock2(odom_lock_);
+  odom_queue_.push_back(*odometryMsg);
+}
+
+void ImageProjection::cloud_handler(const sensor_msgs::msg::PointCloud2::SharedPtr laserCloudMsg)
+{
+  if (!cache_point_cloud(laserCloudMsg)) return;
+
+  if (!deskew_info()) return;
+
+  project_point_cloud();
+
+  cloud_extraction();
+
+  publish_clouds();
+
+  reset_parameters();
+}
+
+bool ImageProjection::cache_point_cloud(const sensor_msgs::msg::PointCloud2::SharedPtr & laserCloudMsg)
+{
+  // cache point cloud
+  cloud_queue_.push_back(*laserCloudMsg);
+  if (cloud_queue_.size() <= 2) return false;
+
+  //    for (sensor_msgs::PointCloud2ConstIterator<uint16_t> it(*laserCloudMsg, "channel");
+  //         it != it.end(); ++it) {
+  //      // TODO: do something with the values of x, y, z
+  //      std::cout << it[0] << std::endl;
+  //    }
+
+  // convert cloud
+  current_cloud_msg_ = std::move(cloud_queue_.front());
+  cloud_queue_.pop_front();
+//  if (sensor == SensorType::VELODYNE || sensor == SensorType::LIVOX) {
+//    pcl::moveFromROSMsg(currentCloudMsg, *laserCloudIn);
+//  }
+  //    else if (sensor == SensorType::OUSTER) {
+  //      // Convert to Velodyne format
+  //      pcl::moveFromROSMsg(currentCloudMsg, *tmpOusterCloudIn);
+  //      laserCloudIn->points.resize(tmpOusterCloudIn->size());
+  //      laserCloudIn->is_dense = tmpOusterCloudIn->is_dense;
+  //      for (size_t i = 0; i < tmpOusterCloudIn->size(); i++) {
+  //        auto & src = tmpOusterCloudIn->points[i];
+  //        auto & dst = laserCloudIn->points[i];
+  //        dst.x = src.x;
+  //        dst.y = src.y;
+  //        dst.z = src.z;
+  //        dst.intensity = src.intensity;
+  //        dst.channel = src.ring;
+  //        dst.time = src.t * 1e-9f;
+  //      }
+  //    }
+  else {
+    RCLCPP_ERROR_STREAM(get_logger(), "Unknown sensor type: " << int(sensor));
+    rclcpp::shutdown();
+  }
+
+  // get timestamp
+  cloudHeader = currentCloudMsg.header;
+  timeScanCur = stamp2Sec(cloudHeader.stamp);
+  timeScanEnd = timeScanCur + laserCloudIn->points.back().time;
+
+  // remove Nan
+  vector<int> indices;
+  pcl::removeNaNFromPointCloud(*laserCloudIn, *laserCloudIn, indices);
+
+  // check dense flag
+  if (laserCloudIn->is_dense == false) {
+    RCLCPP_ERROR(
+      get_logger(), "Point cloud is not in dense format, please remove NaN points first!");
+    rclcpp::shutdown();
+  }
+
+  // check ring channel
+  // we will skip the ring check in case of velodyne - as we calculate the ring value downstream
+  // (line 572)
+  if (ringFlag == 0) {
+    ringFlag = -1;
+    for (int i = 0; i < (int)currentCloudMsg.fields.size(); ++i) {
+      if (currentCloudMsg.fields[i].name == "channel") {
+        ringFlag = 1;
+        break;
+      }
+    }
+    if (ringFlag == -1) {
+      if (sensor == SensorType::VELODYNE) {
+        ringFlag = 2;
+      } else {
+        RCLCPP_ERROR(
+          get_logger(),
+          "Point cloud ring channel not available, please configure your point cloud data!");
+        rclcpp::shutdown();
+      }
+    }
+  }
+
+  // check point time
+  if (deskewFlag == 0) {
+    deskewFlag = -1;
+    for (auto & field : currentCloudMsg.fields) {
+      if (field.name == "time_stamp" || field.name == "t") {
+        deskewFlag = 1;
+        break;
+      }
+    }
+    if (deskewFlag == -1)
+      RCLCPP_WARN(
+        get_logger(),
+        "Point cloud timestamp not available, deskew function disabled, system will drift "
+        "significantly!");
+  }
+
+  return true;
+}
+
+
+
+
+
 } // namespace loam_feature_localization
