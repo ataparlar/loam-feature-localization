e450db08ec5246ebf0f66b20b8d24b27b751f9d1
Ata Parlar <ataparlar@leodrive.ai>
2024-08-23 11:47:49 +0300

    add classes
    Signed-off-by: Ata Parlar <ataparlar@leodrive.ai>

---
+++ include/loam_feature_localization/imu_preintegration.hpp
@@ -0,0 +1,69 @@
+// Copyright 2024 Autoware Foundation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LOAM_FEATURE_LOCALIZATION__IMU_PREINTEGRATION_HPP_
+#define LOAM_FEATURE_LOCALIZATION__IMU_PREINTEGRATION_HPP_
+
+#include "utils.hpp"
+
+#include <Eigen/Geometry>
+#include <opencv2/opencv.hpp>
+#include <rclcpp/rclcpp.hpp>
+
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/pose_with_covariance.hpp>
+#include <nav_msgs/msg/odometry.hpp>
+#include <nav_msgs/msg/path.hpp>
+#include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <std_msgs/msg/float64_multi_array.hpp>
+#include <tf2_ros/transform_broadcaster.h>
+
+#include <boost/filesystem.hpp>
+
+#include <cv_bridge/cv_bridge.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
+#include <pcl/kdtree/kdtree_flann.h>
+
+#include <gtsam/geometry/Rot3.h>
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/navigation/GPSFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
+#include <gtsam/nonlinear/Marginals.h>
+#include <gtsam/nonlinear/Values.h>
+#include <gtsam/inference/Symbol.h>
+#include <gtsam/nonlinear/ISAM2.h>
+
+#include <deque>
+#include <memory>
+#include <string>
+
+namespace loam_feature_localization
+{
+class ImuPreintegration
+{
+  using SharedPtr = std::shared_ptr<ImuPreintegration>;
+  using ConstSharedPtr = const std::shared_ptr<ImuPreintegration>;
+
+};
+}
+
+#endif  // LOAM_FEATURE_LOCALIZATION__IMU_PREINTEGRATION_HPP_
