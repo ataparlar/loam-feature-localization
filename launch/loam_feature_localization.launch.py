a7db13a03d849d85644eb01d1a0e62a6fc8030cf
Ata Parlar <ataparlar@leodrive.ai>
2024-09-06 12:33:27 +0300

    composable
    Signed-off-by: Ata Parlar <ataparlar@leodrive.ai>

--- launch/loam_feature_localization.launch.py
+++ launch/loam_feature_localization.launch.py
@@ -1,27 +1,27 @@
-import os
-
-from ament_index_python import get_package_share_directory
-
 import launch
-
-from launch.actions import DeclareLaunchArgument
-
-from launch.substitutions import LaunchConfiguration
-
-from launch_ros.actions import Node
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+import os
+from ament_index_python import get_package_share_directory
 
 
 def generate_launch_description():
-    """Generate launch description."""
     path_package = get_package_share_directory('loam_feature_localization')
 
     loam_feature_localization_file_param = os.path.join(path_package, 'config/loam_feature_localization.param.yaml')
-    loam_feature_localization_node = Node(
-        package='loam_feature_localization',
-        executable='loam_feature_localization_node',
-        parameters=[loam_feature_localization_file_param]
+    container = ComposableNodeContainer(
+        name='loam_feature_loc',
+        namespace='',
+        package='rclcpp_components',
+        executable='component_container_mt',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='loam_feature_localization',
+                plugin='loam_feature_localization::LoamFeatureLocalization',
+                name='loam_feature_localization',
+                parameters=[loam_feature_localization_file_param])
+        ],
+        output='both',
     )
 
-
-    return launch.LaunchDescription(
-        [loam_feature_localization_node])
+    return launch.LaunchDescription([container])
